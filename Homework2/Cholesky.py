ec0b30f3532d4c2eb6d0d4a1502bdc2cb2dc5d60
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2026-03-01 12:26:23 +0200

    Stopped clustering the console on HW2

--- Homework2/Cholesky.py
+++ Homework2/Cholesky.py
@@ -1,44 +1,57 @@
 import numpy as np
 from numpy.linalg import solve
 
-
-def cholesky(a_matrix, n, epsilon):
+def cholesky(a_matrix, n, epsilon, large_matrix):
     diagonal_array = np.zeros(n, dtype=float)
+
     for p in range(n):
         d_sum = 0
         for k in range(p):
-            d_sum = d_sum + diagonal_array[k] * pow(a_matrix[p,k], 2)
-        print("\nd_sum = ", d_sum)
+            d_sum = d_sum + diagonal_array[k] * (a_matrix[p,k] * a_matrix[p,k]) # Renuntam la pow fiindca e mai eficient sa facem inmultiri
+
+        if not large_matrix:
+            print("\nd_sum = ", d_sum)
 
         diagonal_array[p] = a_matrix[p,p] - d_sum
-        print("\nDiagonal array is: ", diagonal_array[p])
+        if not large_matrix:
+            print("\nDiagonal array element is: ", diagonal_array[p])
 
         if abs(diagonal_array[p]) < epsilon:
-            print("Descompunere imposibila fiindca impartim la 0") #Numarul este foarte aproape de 0
+            print(f"\nDescompunere imposibila la pasul p={p} fiindca impartim la 0") #Numarul este foarte aproape de 0
             break
 
         for i in range(p+1, n):
             l_sum = 0
             for k in range(p):
                 l_sum = l_sum + diagonal_array[k] * a_matrix[i,k] * a_matrix[p,k]
-            print("\nl_sum = ", l_sum)
+
+            if not large_matrix:
+                print("\nl_sum = ", l_sum)
 
             a_matrix[i,p] = (a_matrix[i,p] - l_sum)/diagonal_array[p]
 
-            print("\nModified element in a_matrix: ", a_matrix[i,p])
+            if not large_matrix:
+                print("\nModified element in a_matrix: ", a_matrix[i,p])
 
-        print("\nFinished an iteration of a step, p increased to: ", p+1)
+        if not large_matrix:
+            print("\nFinished an iteration of a step, p increased to: ", p+1)
 
+    if large_matrix:
+        print("Finished computing the Cholesky decomposition")
     return a_matrix, diagonal_array
 
-def cholesky_det(diagonal_array):
+def cholesky_det(diagonal_array, large_matrix):
+
+    if large_matrix:
+        return float("inf")
+
     determinant = 1
     for i in range(len(diagonal_array)):
         determinant = determinant * diagonal_array[i]
 
     return determinant
 
-def cholesky_solve(a_matrix, diagonal_array,n, b_array):
+def cholesky_solve(a_matrix, diagonal_array,n, b_array, large_matrix):
     # L * z = B
     z_unknowns = np.zeros(n)
     y_unknowns = np.zeros(n)
@@ -48,16 +61,19 @@ def cholesky_solve(a_matrix, diagonal_array,n, b_array):
         for j in range(n):
             if i >= j:
                 direct_substitution_sum += a_matrix[i, j] * z_unknowns[j]
-                print("\nDirect substitution sum: ", direct_substitution_sum)
+                if not large_matrix:
+                    print("\nDirect substitution sum: ", direct_substitution_sum)
         z_unknowns[i] = b_array[i] - direct_substitution_sum
-        print(f"\nNecunoscuta Z[{i}]: ", z_unknowns[i])
-        print(f"\nGata iteratia {i}")
+        if not large_matrix:
+            print(f"\nNecunoscuta Z[{i}]: ", z_unknowns[i])
+            print(f"\nGata iteratia {i}")
 
     # D * y = z
     for i in range(n):
         y_unknowns[i] = z_unknowns[i]/diagonal_array[i]
-        print(f"\nNecunoscuta Y[{i}]: ", y_unknowns[i])
-        print(f"\nGata iteratia {i}")
+        if not large_matrix:
+            print(f"\nNecunoscuta Y[{i}]: ", y_unknowns[i])
+            print(f"\nGata iteratia {i}")
 
     # L.transpose * x = y
     a_matrix = a_matrix.transpose()
@@ -66,52 +82,67 @@ def cholesky_solve(a_matrix, diagonal_array,n, b_array):
         for j in range(i+1, n):
             if i <= j:
                 inverted_substitution_sum += a_matrix[i, j] * x_unknowns[j]
-                print("\nInverted substitution sum: ", inverted_substitution_sum)
+                if not large_matrix:
+                    print("\nInverted substitution sum: ", inverted_substitution_sum)
         x_unknowns[i] = y_unknowns[i] - inverted_substitution_sum
-        print(f"\nNecunoscuta X[{i}]: ", x_unknowns[i])
-        print(f"\nGata iteratia {i}")
+        if not large_matrix:
+            print(f"\nNecunoscuta X[{i}]: ", x_unknowns[i])
+            print(f"\nGata iteratia {i}")
 
     a_matrix = a_matrix.transpose()
     return x_unknowns
 
-def multiply_matrix_vector(a_matrix, x_solution, n):
+def multiply_matrix_vector(a_matrix, x_solution, n, large_matrix):
     something_computed = np.zeros(n)
     for i in range(n):
         term_sum = 0
         for j in range(n):
             term_sum += a_matrix[i, j] * x_solution[j]
-        print(f"\nTerm on line {i} is: ", term_sum)
+        if not large_matrix:
+            print(f"\nTerm on line {i} is: ", term_sum)
         something_computed[i] = term_sum
 
     print(f"\nComputed multiplication: ", something_computed)
     return something_computed
 
 def gen_random_matrix(n):
+
+    large_matrix = n > 100
+
     b_matrix = np.random.randn(n, n)
     a_matrix = b_matrix @ b_matrix.T
 
     b_array = np.random.randn(n)
 
-    print("Matricea B:")
-    print(b_matrix)
-    print("\nMatricea A:")
-    print(a_matrix)
+    if not large_matrix:
+        print("Matricea B:")
+        print(b_matrix)
+        print("\nMatricea A:")
+        print(a_matrix)
 
     return a_matrix, b_array
 
 def run():
-    #a_matrix = np.array([[4, 2, 2], [2, 5, 1], [2, 1, 3]], dtype=float)
-    #b_array = np.array([8, 13, 9], dtype=float)
+    '''
+    n = 3
+    epsilon = 1e-15
+    a_matrix = np.array([[4, 2, 2], [2, 5, 1], [2, 1, 3]], dtype=float)
+    b_array = np.array([8, 13, 9], dtype=float)
+    '''
 
-    n = 101
+    n = 1000
     epsilon = 1e-15
     a_matrix, b_array = gen_random_matrix(n)
 
-    print("\nMatricea A initiala:")
-    print(a_matrix)
+    large_matrix = n > 100
 
-    print("\nVector termeni liberi B:")
-    print(b_array)
+    if not large_matrix:
+        print("\nMatricea A initiala:")
+        print(a_matrix)
+
+    if not large_matrix:
+        print("\nVector termeni liberi B:")
+        print(b_array)
 
     init_a_elements = list()
 
@@ -122,16 +153,19 @@ def run():
 
     init_a_elements = np.array(init_a_elements, dtype=float)
 
-    print("\nElementele de sub diagonala principala:")
-    print(init_a_elements)
+    if not large_matrix:
+        print("\nElementele de sub diagonala principala:")
+        print(init_a_elements)
 
-    a_matrix, diagonal_array = cholesky(a_matrix, n, epsilon)
+    a_matrix, diagonal_array = cholesky(a_matrix, n, epsilon, large_matrix)
 
-    print("\nMatricea A updatata este:")
-    print(a_matrix)
+    if not large_matrix:
+        print("\nMatricea A updatata este:")
+        print(a_matrix)
 
-    print("\nVectorul d (Matricea diagonala) este:")
-    print(diagonal_array)
+    if not large_matrix:
+        print("\nVectorul d (Matricea diagonala) este:")
+        print(diagonal_array)
 
     l_elements = list()
 
@@ -142,13 +176,14 @@ def run():
 
     l_elements = np.array(l_elements, dtype=float)
 
-    print("\nElementele de sub diagonala principala dupa update:")
-    print(l_elements)
+    if not large_matrix:
+        print("\nElementele de sub diagonala principala dupa update:")
+        print(l_elements)
 
-    determinant_a_matrix = cholesky_det(diagonal_array)
+    determinant_a_matrix = cholesky_det(diagonal_array, large_matrix)
     print("\nDeterminantul matricei A este: ", determinant_a_matrix)
 
-    system_solution = cholesky_solve(a_matrix, diagonal_array, n, b_array)
+    system_solution = cholesky_solve(a_matrix, diagonal_array, n, b_array, large_matrix)
     print("\nSolutia sistemului este: ", system_solution)
 
     k = 0
@@ -158,10 +193,11 @@ def run():
                 a_matrix[i][j] = init_a_elements[k]
                 k += 1
 
-    print("\nMatricea A reconstruita:")
-    print(a_matrix)
+    if not large_matrix:
+        print("\nMatricea A reconstruita:")
+        print(a_matrix)
 
-    a_init_multi_x_chol = multiply_matrix_vector(a_matrix, system_solution, n)
+    a_init_multi_x_chol = multiply_matrix_vector(a_matrix, system_solution, n, large_matrix)
 
     x_lib = solve(a_matrix, b_array)
 
