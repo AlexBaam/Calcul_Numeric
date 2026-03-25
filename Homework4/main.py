090b3601160a82f3f43b8116b79454b06a4908b3
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2026-03-25 09:48:47 +0200

    Hw 4 done

--- Homework4/main.py
+++ Homework4/main.py
@@ -1,49 +1,114 @@
 from pathlib import Path
 
-def file_content(path):
-    elements_count = {}
+def file_content(file_path):
+    f = open(file_path, 'r')
+    current_count = len(f.readlines()) - 1 # fiindca avem un empty space la final incepem de la -1
+    f.close()
 
-    for file in path.iterdir():
-        if file.is_file():
-            f = open(file, 'r')
-            current_count = len(f.readlines()) - 1 # fiindca avem un empty space la final incepem de la -1
-            f.close()
+    print("The number of elements in " + file_path.parent.name + "_" + file_path.name + " is equal to: " + str(current_count))
+    return current_count
 
-            elements_count[file.name] = current_count
-            print("The number of elements in " + path.name + "_" + file.name + " is equal to: " + str(current_count))
-
-    print("")
-    return elements_count
-
-def nullity_check(path):
+def nullity_check(file_path):
     epsilon = 1e-4
-    found_null = False
 
-    for file in path.iterdir():
-        if file.is_file():
-            f = open(file, 'r')
-            for i in f.readlines():
-                # Aici am nevoie de niste sugestii findca aceasta castare la float(i) reduce precizia, de asta si numarul meu e asa mic pentru epsilon
-                # Ar parea ca numerele cu o perioada foarte mare precum .6666666666666 sunt reduse la .66 indiferent de ce ar avea in urma perioadei, si nu stiu unde poate duce asta la calcule
-                # Exista posibilitatea unor erori daca as avea numere precum 0.0001111111111111111134523 sau ceva, si atunci el nu ar fi nul, dar reducerea acestuia la un nr mai mic de zecimale
-                # Il poate transforma intr-un 0.00 si atunci poate imi zice ca am element nul desi nu am, this is bs tbh
-                if  i != '\n' and (abs(float(i)) < epsilon): # Verific pe baza unei precizii faptul ca am elemente nule
-                    print("There is an element in d0_" + file.name + " that is almost 0, that element is: " + str(i))
-                    found_null = True
-                    break
+    f = open(file_path, 'r')
+    for i in f.readlines():
+        # Aici am nevoie de niste sugestii findca aceasta castare la float(i) reduce precizia, de asta si numarul meu e asa mic pentru epsilon
+        # Ar parea ca numerele cu o perioada foarte mare precum .6666666666666 sunt reduse la .66 indiferent de ce ar avea in urma perioadei, si nu stiu unde poate duce asta la calcule
+        # Exista posibilitatea unor erori daca as avea numere precum 0.0001111111111111111134523 sau ceva, si atunci el nu ar fi nul, dar reducerea acestuia la un nr mai mic de zecimale
+        # Il poate transforma intr-un 0.00 si atunci poate imi zice ca am element nul desi nu am, this is bs tbh
+        if i != '\n' and (abs(float(i)) < epsilon): # Verific pe baza unei precizii faptul ca am elemente nule
+            print("There is an element in d0_" + file_path.name + " that is almost 0, that element is: " + str(i))
+            f.close()
+            return False
 
-    if not found_null:
-        print("There were no nule elements in d0 files")
+    f.close()
+    print("There were no nule elements in d0_" + file_path.name)
+    return True
 
-def diagonals_number(d0_elements_count, d1_elements_count, d2_elements_count):
-    for file_name in d0_elements_count:
-        if file_name in d1_elements_count and file_name in d2_elements_count:
-            p = d0_elements_count[file_name] - d1_elements_count[file_name]
-            q = d0_elements_count[file_name] - d2_elements_count[file_name]
+def read_vector(file_path):
+    values = []
 
-            print("For file " + file_name + " we have: p = " + str(p) + ", q = " + str(q))
-        else:
-            print("File " + file_name + " is missing from d1 or d2")
+    f = open(file_path, 'r')
+    for line in f.readlines():
+        if line != '\n':
+            values.append(float(line))
+    f.close()
+
+    return values
+
+def gauss_seidel(d0, d1, d2, b, n, p, q, epsilon=1e-5, kmax=10000):
+    x = [0.0] * n
+
+    k = 0
+    delta = float('inf')
+
+    while delta >= epsilon and k < kmax:
+        x_old = x.copy()
+        delta = 0.0
+
+        for i in range(n):
+            s = 0.0
+
+            # stanga p
+            if i - p >= 0:
+                s += d1[i - p] * x[i - p]
+
+            # dreapta p
+            if i + p < n:
+                s += d1[i] * x_old[i + p]
+
+            # stanga q
+            if i - q >= 0:
+                s += d2[i - q] * x[i - q]
+
+            # dreapta q
+            if i + q < n:
+                s += d2[i] * x_old[i + q]
+
+            new_val = (b[i] - s) / d0[i]
+
+            diff = abs(new_val - x_old[i])
+            if diff > delta:
+                delta = diff
+
+            x[i] = new_val
+
+        k += 1
+
+        if delta > 1e10:
+            return None, k
+
+    if delta < epsilon:
+        return x, k
+    else:
+        return None, k
+
+def compute_y(d0, d1, d2, x, n, p, q):
+    y = [0.0] * n
+
+    for i in range(n):
+        y[i] += d0[i] * x[i]
+
+        if i < len(d1):
+            y[i] += d1[i] * x[i + p]
+            y[i + p] += d1[i] * x[i]
+
+        if i < len(d2):
+            y[i] += d2[i] * x[i + q]
+            y[i + q] += d2[i] * x[i]
+
+    return y
+
+def infinity_norm(y, b, n):
+    norm = 0.0
+
+    for i in range(n):
+        diff = abs(y[i] - b[i])
+        if diff > norm:
+            norm = diff
+
+    return norm
 
 def run():
     b_path = Path('b')
@@ -51,22 +116,53 @@ def run():
     d1_path = Path('d1')
     d2_path = Path('d2')
 
-    b_elements_count = file_content(b_path)
-    d0_elements_count = file_content(d0_path)
-    d1_elements_count = file_content(d1_path)
-    d2_elements_count = file_content(d2_path)
+    for b_file in b_path.iterdir():
+        if b_file.is_file():
+            file_name = b_file.name
 
-    for file_name in b_elements_count:
-        if file_name in d0_elements_count:
-            if b_elements_count[file_name] != d0_elements_count[file_name]:
-                print(
-                    "The number of elements in b_" + file_name + " and d0_" + file_name + " is not equal, cannot solve the system")
-        else:
-            print("File " + file_name + " exists in b but not in d0")
+            print("Processing file " + file_name)
 
-    nullity_check(d0_path)
+            d0_file = d0_path / file_name
+            d1_file = d1_path / file_name
+            d2_file = d2_path / file_name
 
-    diagonals_number(d0_elements_count, d1_elements_count, d2_elements_count)
+            b_elements_count = file_content(b_file)
+            d0_elements_count = file_content(d0_file)
+            d1_elements_count = file_content(d1_file)
+            d2_elements_count = file_content(d2_file)
 
+            if b_elements_count != d0_elements_count:
+                print("The number of elements in b_" + file_name + " and d0_" + file_name + " is not equal, cannot solve the system")
+                continue
+
+            p = d0_elements_count - d1_elements_count
+            q = d0_elements_count - d2_elements_count
+
+            print("For file " + file_name + " we have: p = " + str(p) + ", q = " + str(q))
+
+            if not nullity_check(d0_file):
+                print("Cannot solve system for file " + file_name)
+                print("")
+                continue
+
+            d0 = read_vector(d0_file)
+            d1 = read_vector(d1_file)
+            d2 = read_vector(d2_file)
+            b = read_vector(b_file)
+
+            x, k = gauss_seidel(d0, d1, d2, b, d0_elements_count, p, q)
+
+            if x is not None:
+                print("Gauss-Seidel converged for file " + file_name + " in " + str(k) + " iterations")
+
+                y = compute_y(d0, d1, d2, x, d0_elements_count, p, q)
+                print("Vector y = A * xGS was computed for file " + file_name)
+
+                norm = infinity_norm(y, b, d0_elements_count)
+                print("The infinity norm ||AxGS - b||∞ for file " + file_name + " is: " + str(norm))
+            else:
+                print("Gauss-Seidel diverged for file " + file_name + " after " + str(k) + " iterations")
+
+            print("")
 if __name__ == "__main__":
     run()
