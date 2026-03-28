9ce9b5d162c062eb41217911dbfc034407369879
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2026-03-28 15:02:23 +0200

    Metoda lui Horner

--- Homework7/main.py
+++ Homework7/main.py
@@ -38,6 +38,35 @@ def coef_deriv(coef):
 
     return deriv
 
+def horner_pol(coef, x):
+    """
+    Pas 3: Aducem polinomul la forma lui Horner ce ia un polinom de forma P(x) = a0 * x^2 + a1 * x + a2  la forma horner(P(x)) = (a0*x + a1)x + a2
+    Asta face calculele sa fie mai simple, nu mai lucram cu puteri, doar inmultiri recursive
+    Intai calculam bucata horner_pol_x = a0*x + a1, urmatoarea recursie calculam horner_pol_x = horner_pol_x*x + a2
+    """
+
+    # Asta e variabila ptr a calcula valoarea polinomului intr-un punct, intai o initializam cu a0 ptr a putea urma forma de mai sus
+    # Ce fac aici e echivalentul lui b0, idk numele de horner_pol_x pare mai intuitiv venind de la horner polinom on X
+    horner_pol_x = coef[0]
+
+    for val in coef[1:]:
+        # Apoi aici fac spre exemplu b1 = a1 + b0 * v
+        # v fiind o valoarea fixa aleasa lui x
+        horner_pol_x = horner_pol_x*x + val
+
+    return horner_pol_x
+
+def horner_all(pol, deriv_pol, second_deriv_pol, x):
+    """
+    Spocul acestei functii este doar sa calculam valoarea pentru un x oarecare a polinomului, polinomuliui derivat si cel dublu derivat
+    """
+
+    pol_x = horner_pol(pol, x)
+    pol_deriv_x = horner_pol(deriv_pol, x)
+    pol_double_deriv_x = horner_pol(second_deriv_pol, x)
+
+    return pol_x, pol_deriv_x, pol_double_deriv_x
+
 def run():
     coef = [1.0, -6.0, 11.0, -6.0]
 
@@ -47,9 +76,20 @@ def run():
 
     print(f"Intervalul in care se gasesc radacinile functiei este: [-{R},{R}]")
 
+    print("Coeficientii polinomului sunt: ", coef)
+
     deriv = coef_deriv(coef)
     second_deriv = coef_deriv(deriv)
 
     print(f"Coeficientii primei derivate sunt: {deriv}")
     print(f"Coeficientii celei de-a doua derivate sunt: {second_deriv}")
+
+    x = 1
+    pol_x, pol_deriv_x, pol_double_deriv_x = horner_all(coef, deriv, second_deriv, x)
+
+    print(f"Pentru x = {x} avem urmatoarele valori ptr polinom: ")
+    print(f"P({x}) = {pol_x}")
+    print(f"P'({x}) = {pol_deriv_x}")
+    print(f"P''({x}) = {pol_double_deriv_x}")
+
 run()
