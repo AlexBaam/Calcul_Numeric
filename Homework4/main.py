54c7711cbee121007b5fba02014da76bd0da0e4f
Acasandrei Nicu-Alexandru <161625952+AlexBaam@users.noreply.github.com>
2026-03-28 13:51:25 +0200

    Some comments

--- Homework4/main.py
+++ Homework4/main.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-
+'''
+Functie folosita pentru a citi continutul unui fisier si a returna lungimea acestui fisier 
+Luam in considerare faptul ca pe fiecare rand avem un singur numar 
+'''
 def file_content(file_path):
     f = open(file_path, 'r')
     current_count = len(f.readlines()) - 1 # fiindca avem un empty space la final incepem de la -1
@@ -8,8 +11,13 @@ def file_content(file_path):
     print("The number of elements in " + file_path.parent.name + "_" + file_path.name + " is equal to: " + str(current_count))
     return current_count
 
+'''
+Functie folosita pentru a verifica daca inantrul unor fisiere avem elemente nule
+Facem asta prin comparatie cu epsilon ce e foarte mic
+Ca sa nu verificam ultima linie sau linii ce pot fi goale folosim i != endline
+'''
 def nullity_check(file_path):
-    epsilon = 1e-4
+    epsilon = 1e-8
 
     f = open(file_path, 'r')
     for i in f.readlines():
@@ -26,64 +34,82 @@ def nullity_check(file_path):
     print("There were no nule elements in d0_" + file_path.name)
     return True
 
+'''
+Functie ce ia valorile dintr-un fisier si le pune intr-un vector (lista)
+Ne trebuie pentru gass-siedel
+'''
 def read_vector(file_path):
     values = []
 
     f = open(file_path, 'r')
     for line in f.readlines():
-        if line != '\n':
+        if line != '\n': # Ignoram spatiile libere otherwise it will mess it up
             values.append(float(line))
     f.close()
 
     return values
 
+'''
+Functie in care aplicam metoda gauss-seidel pentru a rezolva sistemul Ax = b
+Fiindca nu putem memora metricea complet folosim vecotri ptr fiecare dintre diagonale si vectorul b
+n e marimea matricei (stabilita ptr intermediul dimensiunii d0), p si q sunt distantele diagonalelor fata de cea principala
+kmax e nr maxim de iteratii ce il permitem, daca nu il oferim in cod e by default 10000
+Epsilon e toleranta permisa intre 2 iteratii
+Adica daca diferenta dintre 2 iteratii e prea apropiata de epsilon inseamna ca noi divergem'''
 def gauss_seidel(d0, d1, d2, b, n, p, q, epsilon=1e-5, kmax=10000):
+    # Vector solutie ce e initializat cu 0
     x = [0.0] * n
 
     k = 0
-    delta = float('inf')
+    delta = float('inf') # Ne asiguram ca bucla incepe macar o data fiindca daca delta e 0 atunci epsilon e mai mare
+    # Reprezinta diferenta dintre doua iteratii consecutive
 
     while delta >= epsilon and k < kmax:
+        # Aici salvam iteratia veche a algoritmului (ultimul vector solutie) ptr ca urmeaza sa updatam x
         x_old = x.copy()
-        delta = 0.0
+        delta = 0.0 # In cadrul fiecarei iteratii readucem la 0 pentru a putea reface comparatiile
 
         for i in range(n):
             s = 0.0
 
-            # stanga p
             if i - p >= 0:
                 s += d1[i - p] * x[i - p]
 
-            # dreapta p
             if i + p < n:
                 s += d1[i] * x_old[i + p]
 
-            # stanga q
             if i - q >= 0:
                 s += d2[i - q] * x[i - q]
 
-            # dreapta q
             if i + q < n:
                 s += d2[i] * x_old[i + q]
 
             new_val = (b[i] - s) / d0[i]
 
+            # DAca diferenta dintre modul de noua solutie si cea veche este mai mare decat delta atunci delta devine acea valoare
             diff = abs(new_val - x_old[i])
             if diff > delta:
                 delta = diff
 
             x[i] = new_val
 
+        # Incrementam numarul de iteratii realizat pana acum
         k += 1
 
+        # Divergem in cazul in care delta devine prea mare (Peste 10^10)
         if delta > 1e10:
             return None, k
 
+    # Daca atunci cand iesim din while delta e mai mic decat epsilon inseamna ca nu am ajuns la a diverge, deci returnam solutia
     if delta < epsilon:
         return x, k
     else:
         return None, k
 
+'''
+Functie pentru a calcula y = A * xGS (solutia Gauss-Siedel)
+Acesta trebuie sa fie aproape de b ca valori
+'''
 def compute_y(d0, d1, d2, x, n, p, q):
     y = [0.0] * n
 
@@ -100,6 +126,9 @@ def compute_y(d0, d1, d2, x, n, p, q):
 
     return y
 
+'''
+Verificam diferenta dintre b si y ptr a vedea daca normele sunt destul de apropiate
+'''
 def infinity_norm(y, b, n):
     norm = 0.0
 
@@ -156,10 +185,9 @@ def run():
                 print("Gauss-Seidel converged for file " + file_name + " in " + str(k) + " iterations")
 
                 y = compute_y(d0, d1, d2, x, d0_elements_count, p, q)
-                print("Vector y = A * xGS was computed for file " + file_name)
 
                 norm = infinity_norm(y, b, d0_elements_count)
-                print("The infinity norm ||AxGS - b||∞ for file " + file_name + " is: " + str(norm))
+                print("The norm ||AxGS - b|| for file " + file_name + " is: " + str(norm))
             else:
                 print("Gauss-Seidel diverged for file " + file_name + " after " + str(k) + " iterations")
 
